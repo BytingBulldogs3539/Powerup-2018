122a47fb22b287470ce149ff32c89b49f11c49b0
IVan <bbprogrammers3539@gmail.com>
2018-01-20 13:29:06 -0500

    Actually Fix DriveTrain
    Cameron

--- src/org/usfirst/frc/team3539/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team3539/robot/subsystems/Drivetrain.java
@@ -22,28 +22,6 @@ public class Drivetrain extends Subsystem
 		rf = new TalonSRX(RobotMap.rf);
 		rb = new TalonSRX(RobotMap.rb);
 
-		lf.set(ControlMode.PercentOutput, 0);
-		rf.set(ControlMode.PercentOutput, 0);
-
-		// rf.changeControlMode(TalonControlMode.PercentVbus);
-		// rbMotor.changeControlMode(TalonControlMode.PercentVbus);
-		// lbMotor.changeControlMode(TalonControlMode.PercentVbus);
-		// rb.changeControlMode(TalonControlMode.Follower);
-		// lb.changeControlMode(TalonControlMode.Follower);
-
-		rb.set(ControlMode.Follower, rf.getDeviceID());
-		lf.set(ControlMode.Follower, lb.getDeviceID());
-
-		// lf.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
-		// rf.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
-		// rbMotor.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
-		// lbMotor.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
-
-		// lf.configOutputVoltage(0.0f, -0.0f);
-		// lf.configPeakOutputVoltage(12.0f, -12.0f);
-		// rf.configNominalOutputVoltage(0.0f, -0.0f);
-		// rf.configPeakOutputVoltage(12.0f, -12.0f);
-
 		lf.configPeakOutputForward(1, 1);
 		lf.configPeakOutputReverse(-1, 1);
 		rf.configPeakOutputForward(1, 1);
@@ -54,21 +32,17 @@ public class Drivetrain extends Subsystem
 		rb.configPeakOutputForward(1, 1);
 		rb.configPeakOutputReverse(-1, 1);
 
-		// lf.configMaxOutputVoltage(12);
-		// rf.configMaxOutputVoltage(12);
-		// rf.setCurrentLimit(40);
-		// lf.setCurrentLimit(40);
 
 		lf.configNominalOutputForward(0, 1);
 		rf.configNominalOutputForward(0, 1);
 		lb.configNominalOutputForward(0, 1);
 		rb.configNominalOutputForward(0, 1);
-		lf.configContinuousCurrentLimit(35, 0);
-		rf.configContinuousCurrentLimit(35, 0);
+		lf.configNominalOutputReverse(0, 1);
+		rf.configNominalOutputReverse(0, 1);
+		lb.configNominalOutputReverse(0, 1);
+		rb.configNominalOutputReverse(0, 1);
 
-		lf.enableCurrentLimit(true);
-		rf.enableCurrentLimit(true);
-		drive = new Drive(rf,lb);
+		drive = new Drive(rf,rb,lf,lb);
 	}
 	
 	public void driveArcade(double forward, double rotate)
