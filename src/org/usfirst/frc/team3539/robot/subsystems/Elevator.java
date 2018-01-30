b5a527e2c139e7dafc60524f17e367f19d3a7220
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2018-01-30 18:27:22 -0500

    Remove Unusable Code

--- src/org/usfirst/frc/team3539/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team3539/robot/subsystems/Elevator.java
@@ -15,8 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Elevator extends Subsystem
 {
-	private static LightBeam beam;
-	TalonSRX lLift, rLift;
+	TalonSRX liftTalon1, liftTalon2;
 	int error = 1000;
 	int loopAmount = 0;
 	int loopCounter = 0;
@@ -24,82 +23,51 @@ public class Elevator extends Subsystem
 
 	public Elevator()
 	{
-		beam = new LightBeam(0);
-		lLift = new TalonSRX(RobotMap.l1LifterMotor);
-
-		rLift = new TalonSRX(RobotMap.r1LifterMotor);
-
+		liftTalon1 = new TalonSRX(RobotMap.l1LifterMotor);
+		liftTalon2 = new TalonSRX(RobotMap.r1LifterMotor);
+		configMotors();
+		setMotorPower(0);
 	}
 
 	public void setMotorPower(double speed)
 	{
-		lLift.set(ControlMode.PercentOutput, speed);
-
-		rLift.set(ControlMode.PercentOutput, -speed);
-
+		liftTalon1.set(ControlMode.PercentOutput, speed);
+		liftTalon2.set(ControlMode.PercentOutput, speed);
 	}
-
-	public boolean isTriggered()
+	
+	public void configMotors()
 	{
-		return beam.get();
+		liftTalon2.set(ControlMode.Follower, liftTalon1.getDeviceID());
 	}
 
 	@Override
 	protected void initDefaultCommand()
 	{
-		// setDefaultCommand(new ElevatorCommand()); this stuff needs to be fixed
 	}
 
 	public void zeroEnc()
 	{
-		lLift.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-		rLift.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
-		lLift.getSensorCollection().setPulseWidthPosition(0, 10);
-		rLift.getSensorCollection().setPulseWidthPosition(0, 10);
-		lLift.setSelectedSensorPosition(lLift.getSelectedSensorPosition(10), 0, 10);
-		rLift.setSelectedSensorPosition(rLift.getSelectedSensorPosition(10), 0, 10);
+		liftTalon1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 10);
+		liftTalon1.getSensorCollection().setPulseWidthPosition(0, 10);
+		liftTalon1.setSelectedSensorPosition(liftTalon1.getSelectedSensorPosition(10), 0, 10);
 	}
 
 	public void setPID(double P, double I, double D, double F)
 	{
-		lLift.config_kF(0, F, 10);
-		rLift.config_kF(0, F, 10);
-		lLift.config_kP(0, P, 10);
-		rLift.config_kP(0, P, 10);
-		lLift.config_kI(0, I, 10);
-		rLift.config_kI(0, I, 10);
-		lLift.config_kD(0, D, 10);
-		rLift.config_kD(0, D, 10);
+		liftTalon1.config_kF(0, F, 10);
+		liftTalon1.config_kP(0, P, 10);
+		liftTalon1.config_kI(0, I, 10);
+		liftTalon1.config_kD(0, D, 10);
 	}
 
 	public void setSetpointLift(double setpointinches)
 	{
-		zeroEnc();
-		lLift.set(ControlMode.Position, setpointinches);
-		rLift.set(ControlMode.Position, -setpointinches);
-	}
-
-	public void DriveSpeed(double Velocity)
-	{
-		lLift.set(ControlMode.Velocity, ftpsToEncps(Velocity));
-		rLift.set(ControlMode.Velocity, ftpsToEncps(-Velocity));
-	}
-
-	public double ftpsToEncps(double ftps) // will probably be changed
-	{
-
-		return ftps * 394;
-		// return ftps * 100.0;
-	}
-
-	public double degreesToEnc(double degrees)
-	{
-		return inchToEncoder((RobotMap.robotCir / 360) * degrees);
+		liftTalon1.set(ControlMode.Position, setpointinches);
 	}
 
 	public boolean onTarget()
 	{
-		if (lLift.getClosedLoopError(0) <= allowedError && lLift.getClosedLoopError(0) >= -allowedError && rLift.getClosedLoopError(0) <= allowedError && rLift.getClosedLoopError(0) >= -allowedError)
+		if (liftTalon1.getClosedLoopError(0) <= allowedError && liftTalon1.getClosedLoopError(0) >= -allowedError && liftTalon2.getClosedLoopError(0) <= allowedError && liftTalon2.getClosedLoopError(0) >= -allowedError)
 		{
 			loopCounter++;
 			System.out.println(loopCounter);
@@ -121,8 +89,8 @@ public class Elevator extends Subsystem
 
 	public void setTargetAllowedError(int MotorTicks)
 	{
-		lLift.configAllowableClosedloopError(0, MotorTicks, 10);
-		rLift.configAllowableClosedloopError(0, MotorTicks, 10);
+		liftTalon1.configAllowableClosedloopError(0, MotorTicks, 10);
+		liftTalon2.configAllowableClosedloopError(0, MotorTicks, 10);
 		allowedError = MotorTicks;
 	}
 
@@ -131,15 +99,14 @@ public class Elevator extends Subsystem
 		loopCounter = 0;
 	}
 
-	public double inchToEncoder(double inches)
+	public double inchToEncoder(double inches)// Will Be A Different Conversion ration.
 	{
 		return (inches / 12.56) * 4096;
 	}
 
 	public void updateEnc()
 	{
-		SmartDashboard.putNumber("Right Elevator Enc", rLift.getSelectedSensorPosition(0));
-		SmartDashboard.putNumber("Left Elevator Enc", lLift.getSelectedSensorPosition(0));
+		SmartDashboard.putNumber("Left Elevator Enc", liftTalon1.getSelectedSensorPosition(0));
 	}
 
 }
