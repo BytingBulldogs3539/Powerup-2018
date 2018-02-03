78ffc01e1013c1208792bebe85d9173becb8fdff
Dulak <ddominik@live.com>
2018-02-03 14:43:49 -0500

    revert command

--- src/org/usfirst/frc/team3539/robot/commands/MotionProfileCommand.java
+++ src/org/usfirst/frc/team3539/robot/commands/MotionProfileCommand.java
@@ -21,13 +21,7 @@ public class MotionProfileCommand extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     	Robot.motion.Control();	
-    	Robot.motion.Control();	
-    	Robot.motion.Control();	
-    	Robot.motion.Control();	
-    	Robot.motion.Control();	
-    	Robot.motion.Control();	
-    	Robot.motion.Control();	
-    	Robot.motion.Control();	
+    	
 
     	Robot.motion.startMotionProfile();
 
