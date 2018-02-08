25ff64861110cee3b3509d1c29aea99b12164278
Dulak <ddominik@live.com>
2018-02-07 20:03:38 -0500

    add AutonMotionProfileCommand

--- src/org/usfirst/frc/team3539/robot/autoncommands/AutonMotionProfileCommand.java
+++ src/org/usfirst/frc/team3539/robot/autoncommands/AutonMotionProfileCommand.java
@@ -1,21 +1,19 @@
-package org.usfirst.frc.team3539.robot.commands;
+package org.usfirst.frc.team3539.robot.autoncommands;
 
 import org.usfirst.frc.team3539.robot.Robot;
-import org.usfirst.frc.team3539.robot.RobotMap;
-import org.usfirst.frc.team3539.robot.subsystems.MotionProfile;
-
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class MotionProfileCommand extends Command {
+public class AutonMotionProfileCommand extends Command {
 
-    public MotionProfileCommand() {
+    public AutonMotionProfileCommand() {
         // Use requires() here to declare subsystem dependencies
-      requires(Robot.motion);
+        // eg. requires(chassis);
+        requires(Robot.motion);
+
     }
 
     // Called just before this Command runs the first time
@@ -24,28 +22,17 @@ public class MotionProfileCommand extends Command {
     	Robot.motion.Reset();
     	Robot.motion.setMotionProfile();
     	System.out.println("-----motion profile mode set on talon----");
-    	Robot.motion.Process();
+    	Robot.motion.Process();   	
+    	Robot.motion.startMotionProfile();
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
-   	Robot.motion.Control();	
-  	Robot.motion.setMotionProfile();
-   	
-  	//if (Robot.m_oi.one.buttonX.get())
-  		//Robot.motion.reset();
-  	if(Robot.oi.one.buttonB.get()&&!Robot.motion.start())
-  	{
-   	//	System.out.println("-------------------------");
-   		Robot.motion.startMotionProfile();
-   	}else
-   	{
-   		Robot.motion.SetMotionFalse();
-   	}
-  	if (Robot.oi.one.buttonX.get())
-    	Robot.motion.Reset();
-    	//Robot.motion.print();
+       	Robot.motion.Control();
+       	//
+      	//Robot.motion.setMotionProfile(); possibly should be here
+
 
     }
 
@@ -56,11 +43,14 @@ public class MotionProfileCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+   		Robot.motion.SetMotionFalse();
+    	Robot.motion.Reset();
+
+
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	end();
     }
 }
