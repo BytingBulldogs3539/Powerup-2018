eb71f3f2d7b4913543757c47e9ab0059171e65d3
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2018-02-08 21:18:03 -0500

    add Serial

--- src/org/usfirst/frc/team3539/robot/Robot.java
+++ src/org/usfirst/frc/team3539/robot/Robot.java
@@ -5,6 +5,7 @@ import org.usfirst.frc.team3539.robot.subsystems.Elevator;
 import org.usfirst.frc.team3539.robot.subsystems.Intake;
 import org.usfirst.frc.team3539.robot.subsystems.LateralPitch;
 import org.usfirst.frc.team3539.robot.subsystems.MotionProfile;
+import org.usfirst.frc.team3539.robot.subsystems.SerialSub;
 
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
@@ -18,20 +19,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as described in the IterativeRobot documentation. If you change the name of this class or the package after creating this
+ * project, you must also update the manifest file in the resource directory.
  **/
 
-public class Robot extends IterativeRobot {
-	public static  MotionProfile motion = new MotionProfile();
+public class Robot extends IterativeRobot
+{
+	public static MotionProfile motion = new MotionProfile();
 	// SUBSYSTEMS
 	public static DriveTrain driveTrain = new DriveTrain();
 	public static Intake intake = new Intake();
 	public static Elevator elevator = new Elevator();
 	public static LateralPitch pitch = new LateralPitch();
+	public static SerialSub serialSub = new SerialSub();
 
 	public static PowerDistributionPanel pdp = new PowerDistributionPanel();
 	public static Compressor c;
@@ -43,73 +43,87 @@ public class Robot extends IterativeRobot {
 	SendableChooser<Command> allianceChooser = new SendableChooser<Command>();
 	SendableChooser<Command> autonChooser = new SendableChooser<Command>();
 
-	public void robotInit() {
+	public void robotInit()
+	{
 		oi = new OI();
 		SmartInit();
 
-		try {
+		try
+		{
 			cameraOne = CameraServer.getInstance().startAutomaticCapture(0);
 			cameraOne.setResolution(480, 360);
 			cameraTwo = CameraServer.getInstance().startAutomaticCapture(1);
 			cameraTwo.setResolution(480, 360);
-		} catch (Error e) {
+		}
+		catch (Error e)
+		{
 		}
 	}
 
 	/**
-	 * The VM is configured to automatically run this class, and to call the
-	 * functions corresponding to each mode, as described in the TimedRobot
-	 * documentation. If you change the name of this class or the package after
-	 * creating this project, you must also update the build.properties file in the
-	 * project.
+	 * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as described in the TimedRobot documentation. If you change the name of this class or the package after creating this
+	 * project, you must also update the build.properties file in the project.
 	 */
 
 	@Override
-	public void disabledInit() {
+	public void disabledInit()
+	{
 		Scheduler.getInstance().run();
 
 	}
 
-	public void disabledPeriodic() {
+	public void disabledPeriodic()
+	{
+		serialSub.getDistance(2);
 
 		Scheduler.getInstance().run();
 	}
 
-	public void autonomousInit() {
+	public void autonomousInit()
+	{
 		Robot.driveTrain.calibrateGyro();
 		String gameData;
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
 
-		if (gameData.charAt(0) == 'L') {
+		if (gameData.charAt(0) == 'L')
+		{
 			System.out.println("left auton");
 			// Put left auto code here
-		} else {
+		}
+		else
+		{
 			System.out.println("right auton");
 			// Put right auto code here
 		}
 
 		autonMode = (Command) autonChooser.getSelected();
-		if (autonMode != null) {
+		if (autonMode != null)
+		{
 			autonMode.start();
 		}
 	}
 
-	public void autonomousPeriodic() {
+	public void autonomousPeriodic()
+	{
 		Scheduler.getInstance().run();
 	}
 
-	public void teleopInit() {
+	public void teleopInit()
+	{
 	}
 
-	public void teleopPeriodic() {
+	public void teleopPeriodic()
+	{
 		Robot.driveTrain.updateEncoders();
 		Scheduler.getInstance().run();
 	}
 
-	public void testPeriodic() {
+	public void testPeriodic()
+	{
 	}
 
-	public void SmartInit() {
+	public void SmartInit()
+	{
 
 		positionChooser.addObject("Left", null);
 		positionChooser.addDefault("Middle", null);
---
+++ src/org/usfirst/frc/team3539/robot/subsystems/SerialSub.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team3539.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class SerialSub extends Subsystem
+{
+	SerialPort arduino = new SerialPort(9600, SerialPort.Port.kUSB1);
+
+	public void initDefaultCommand()
+	{
+	}
+
+	public String readDistAsString()
+	{
+		return arduino.readString();
+	}
+
+	public double getDistance(int value)
+	{
+		String[] words = readDistAsString().split(",");
+		return Double.parseDouble(words[value]);
+	}
+}
