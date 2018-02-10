adc644202ec0010d752acdbd5f30a7185ebe6a13
BytingBulldogs3539 <3539rasputin@gmail.com>
2018-02-10 15:14:43 -0500

    Update LateralPitch

--- src/org/usfirst/frc/team3539/robot/subsystems/LateralPitch.java
+++ src/org/usfirst/frc/team3539/robot/subsystems/LateralPitch.java
@@ -10,13 +10,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class LateralPitch extends Subsystem
 {
-	TalonSRX pitch;
-	int pitchTimeout = 10;
-	int pidIdx = 0;
-	int error = 1000;
-	int loopAmount = 0;
-	int loopCounter = 0;
-	int allowedError = 0;
+	private TalonSRX pitch;
 
 	public enum PitchAngle
 	{
@@ -27,21 +21,21 @@ public class LateralPitch extends Subsystem
 	{
 		pitch = new TalonSRX(RobotMap.pitch);
 
-		pitch.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, pidIdx, pitchTimeout);
-		pitch.config_kP(pidIdx, RobotMap.pitchPea, pitchTimeout);
-		pitch.config_kI(pidIdx, RobotMap.pitchEye, pitchTimeout);
-		pitch.config_kD(pidIdx, RobotMap.pitchDee, pitchTimeout);
+		pitch.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 10);
+		
+		pitch.config_kP(0, RobotMap.pitchPea, 10);
+		pitch.config_kI(0, RobotMap.pitchEye, 10);
+		pitch.config_kD(0, RobotMap.pitchDee, 10);
 
-		pitch.configNominalOutputForward(0, pitchTimeout);
+		pitch.configNominalOutputForward(0, 10);
 
-		pitch.configNominalOutputReverse(0, pitchTimeout);
+		pitch.configNominalOutputReverse(0, 10);
 
-		pitch.configPeakOutputReverse(-1, pitchTimeout);
-		pitch.configPeakOutputForward(1, pitchTimeout);
+		pitch.configPeakOutputReverse(-1, 10);
+		pitch.configPeakOutputForward(1, 10);
 
-		pitch.configForwardSoftLimitThreshold(RobotMap.pitchLimitUp, pitchTimeout);
-		pitch.configReverseSoftLimitThreshold(RobotMap.pitchLimitDown, pitchTimeout);
-		pitch.configAllowableClosedloopError(pidIdx, 1000, pitchTimeout);
+		pitch.configForwardSoftLimitThreshold(RobotMap.pitchLimitUp, 10);
+		pitch.configReverseSoftLimitThreshold(RobotMap.pitchLimitDown, 10);
 	}
 
 	public void movePitchUp()
@@ -54,46 +48,47 @@ public class LateralPitch extends Subsystem
 		pitch.set(ControlMode.PercentOutput, -.20);
 	}
 
-	public void setLateralPID(double p, double i, double d)
+	public void setPID(double P, double I, double D)
 	{
-		pitch.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, pidIdx, pitchTimeout);
-		pitch.config_kP(pidIdx, p, pitchTimeout);
-		pitch.config_kI(pidIdx, i, pitchTimeout);
-		pitch.config_kD(pidIdx, d, pitchTimeout);
+		pitch.config_kP(0, P, 10);
+		
+		pitch.config_kI(0, I, 10);
+		
+		pitch.config_kD(0, D, 10);
 	}
 
-	public void setPosition(PitchAngle position)
+	public void setSetpointPitch(PitchAngle pitchAngle)
 	{
-		if (position == PitchAngle.DOWN)
+		if (pitchAngle == PitchAngle.DOWN)
 		{
 			pitch.set(ControlMode.Position, RobotMap.pitchEncPosDown);
 		}
-		else if (position == PitchAngle.UP)
+		else if (pitchAngle == PitchAngle.UP)
 		{
 			pitch.set(ControlMode.Position, RobotMap.pitchEncPosUp);
 		}
-		else if (position == PitchAngle.INTAKE)
+		else if (pitchAngle == PitchAngle.INTAKE)
 		{
 			pitch.set(ControlMode.Position, RobotMap.pitchEncPosIntake);
-
 		}
 	}
 
-	public double getEncPos()
-	{
-		return pitch.getSelectedSensorPosition(0);
-	}
+	private int maxLoopNumber = 0;
+	private int onTargetCounter = 0;
+	private int allowedErrorRange = 0;
 
 	public boolean onTarget()
 	{
-		if (pitch.getClosedLoopError(0) <= allowedError && pitch.getClosedLoopError(0) >= -allowedError)
+		if (Math.abs(pitch.getClosedLoopError(0)) <= allowedErrorRange)
 		{
-			loopCounter++;
-			System.out.println(loopCounter);
+			onTargetCounter++;
 		}
 		else
-			loopCounter = 0;
-		if (loopCounter >= loopAmount)
+		{
+			onTargetCounter = 0;
+		}
+
+		if (maxLoopNumber <= onTargetCounter)
 		{
 			return true;
 		}
@@ -101,20 +96,15 @@ public class LateralPitch extends Subsystem
 		return false;
 	}
 
-	public void setLoopOnTarget(int LoopAmount)
+	public void setupOnTarget(int ticks, int maxLoopNumber)
 	{
-		loopAmount = LoopAmount;
-	}
-
-	public void setTargetAllowedError(int MotorTicks)
-	{
-		pitch.configAllowableClosedloopError(0, MotorTicks, 10);
-		allowedError = MotorTicks;
-	}
-
-	public void zeroLoopCounter()
-	{
-		loopCounter = 0;
+		// zero the on target counter
+		onTargetCounter = 0;
+		
+		pitch.configAllowableClosedloopError(0, ticks, 10);
+		
+		// set tolerance in ticks
+		allowedErrorRange = ticks;
 	}
 
 	@Override
