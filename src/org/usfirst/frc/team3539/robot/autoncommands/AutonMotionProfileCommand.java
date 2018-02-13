3f19172a84bfe8346482e7c08f7103d34d26f133
BytingBulldogs3539 <3539rasputin@gmail.com>
2018-02-13 18:00:24 -0500

    Update MotionProfileCommand

--- src/org/usfirst/frc/team3539/robot/autoncommands/AutonMotionProfileCommand.java
+++ src/org/usfirst/frc/team3539/robot/autoncommands/AutonMotionProfileCommand.java
@@ -2,8 +2,6 @@ package org.usfirst.frc.team3539.robot.autoncommands;
 
 import org.usfirst.frc.team3539.robot.Robot;
 
-import com.sun.glass.ui.Timer;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -11,16 +9,11 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class AutonMotionProfileCommand extends Command
 {
-
 	public AutonMotionProfileCommand()
 	{
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
 		requires(Robot.driveTrain);
-
 	}
 
-	// Called just before this Command runs the first time
 	protected void initialize()
 	{
 		Robot.driveTrain.DisabledMotionProfile();
@@ -30,32 +23,26 @@ public class AutonMotionProfileCommand extends Command
 		Robot.driveTrain.setMotionProfile();
 		System.out.println("-----motion profile mode set on talon----");
 		Robot.driveTrain.startFilling();
-	//	edu.wpi.first.wpilibj.Timer.delay(10);
+		// edu.wpi.first.wpilibj.Timer.delay(10);
 	}
 
-	// Called repeatedly when this Command is scheduled to run
 	protected void execute()
-	{System.out.println("printing in execute");
-		//
-	Robot.driveTrain.setMotionProfile();
-
+	{
+		System.out.println("printing in execute");
+		
+		Robot.driveTrain.setMotionProfile();
 	}
 
-	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished()
 	{
 		return Robot.driveTrain.GetFinish();
 	}
 
-	// Called once after isFinished returns true
 	protected void end()
 	{
-		Robot.driveTrain.MotionProfileReset();
-
+		Robot.driveTrain.MotionProfileReset();	
 	}
-
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
+	
 	protected void interrupted()
 	{
 		end();
