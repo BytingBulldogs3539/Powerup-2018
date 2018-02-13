e3a9245ef848d3341dea2c7d9b701de8fb884805
BytingBulldogs3539 <3539rasputin@gmail.com>
2018-02-13 17:26:02 -0500

    Enable Solenoids

--- src/org/usfirst/frc/team3539/robot/subsystems/Solenoids.java
+++ src/org/usfirst/frc/team3539/robot/subsystems/Solenoids.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3539.robot.subsystems;
 
 import org.usfirst.frc.team3539.robot.RobotMap;
 //import org.usfirst.frc.team3539.robot.utilities.bbDoubleSolenoid;
+import org.usfirst.frc.team3539.robot.utilities.bbDoubleSolenoid;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -10,20 +11,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Solenoids extends Subsystem
 {
-//	public bbDoubleSolenoid intakeSol;
-//	public bbDoubleSolenoid lifterSol;
-//	public bbDoubleSolenoid rampSol;
-//	public bbDoubleSolenoid shifter;
+	public bbDoubleSolenoid intakeSol;
+	public bbDoubleSolenoid lifterSol;
+	public bbDoubleSolenoid rampSol;
+	public bbDoubleSolenoid shifterSol;
 	
 
 	public Solenoids()
 	{
-//		intakeSol = new bbDoubleSolenoid(RobotMap.intakesolOn, RobotMap.intakesolOff, false);
-//		lifterSol = new bbDoubleSolenoid(RobotMap.liftersolOn, RobotMap.liftersolOff, false);
-//		rampSol = new bbDoubleSolenoid(RobotMap.rampsolOn, RobotMap.rampsolOff, false);
-//		shifter = new bbDoubleSolenoid(RobotMap.shiftersolOn, RobotMap.shiftersolOff, false);
+		intakeSol = new bbDoubleSolenoid(RobotMap.intakesolOn, RobotMap.intakesolOff, false);
+		lifterSol = new bbDoubleSolenoid(RobotMap.liftersolOn, RobotMap.liftersolOff, false);
+		rampSol = new bbDoubleSolenoid(RobotMap.rampsolOn, RobotMap.rampsolOff, false);
+		shifterSol = new bbDoubleSolenoid(RobotMap.shiftersolOn, RobotMap.shiftersolOff, false);
 	}
-	//make new solinoid call it shifter and make it generic on and off ports like 3539 in robot map make instant command to toggle
 
 	public void initDefaultCommand()
 	{
