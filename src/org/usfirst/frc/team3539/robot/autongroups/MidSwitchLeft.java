9340260a01f7c156b933abd0f8f48bc2c8399110
Devolian <devondoyle@outlook.com>
2018-04-13 12:44:48 -0400

    ready to test left auto

--- src/org/usfirst/frc/team3539/robot/autongroups/MidSwitchLeft.java
+++ src/org/usfirst/frc/team3539/robot/autongroups/MidSwitchLeft.java
@@ -30,7 +30,7 @@ public class MidSwitchLeft extends CommandGroup
 		addSequential(new AutonIntakeCommand(-.7, 1));
 		
 		addSequential(new AutonMotionProfileEx(MiddleLeft2.PointsR,MiddleLeft2.PointsL,MiddleLeft2.kNumPoints));
-		addParallel( new AutonElevatorArm(30,100,0,0));// change to 0 when confident 
+		addParallel( new AutonElevatorArm(30,105,0,0));// change to 0 when confident 
 		addSequential( new AutonWaitCommand(0.10));
 		addParallel( new AutonIntakeCommand(1, 5));
 		
