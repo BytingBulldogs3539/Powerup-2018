e79f8cf14c17489ab6022571f14c1d362c3ac251
Dulak <ddominik@live.com>
2018-06-12 22:53:34 -0400

    fix spelling

--- src/org/usfirst/frc/team3539/robot/commands/IntakeCommand.java
+++ src/org/usfirst/frc/team3539/robot/commands/IntakeCommand.java
@@ -5,7 +5,8 @@ import org.usfirst.frc.team3539.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * utilizes the intake subsystem to intake 
+ * implemnts a 
  */
 public class IntakeCommand extends Command
 {
@@ -22,8 +23,10 @@ public class IntakeCommand extends Command
 	protected void execute()
 	{
 		double intakeSpeed = -Robot.oi.two.getLeftTrigger() + Robot.oi.two.getRightTrigger();
-		intakeSpeed = Math.max(intakeSpeed, -.6);
-		Robot.intake.setMotorPower(Math.copySign(Math.sqrt(Math.abs(intakeSpeed)), intakeSpeed));
+		intakeSpeed = Math.copySign(Math.sqrt(Math.abs(intakeSpeed)), intakeSpeed); // applies a root function then copies the sign of the value prior to root
+		intakeSpeed = Math.max(intakeSpeed, -.75);// caps the intaking speed to 75%
+		
+		Robot.intake.setMotorPower(intakeSpeed);
 	
 	}
 
